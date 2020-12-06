f50b8aacac8915750a3882cc1f1e59894b0c1a57
Kristofer Karl Jensson <46908002+krisjensson@users.noreply.github.com>
2020-12-06 14:07:08 -0800

    Update pages.js

--- src/pages.js
+++ src/pages.js
@@ -9,8 +9,20 @@ const pages = [
         "This website is out to show you that our planet does not have a lot left if we dont change our ways of living.",
       heading_icon: "pe-7s-car icon-gradient bg-mean-fruit",
     },
+    template: "./src/wherewestand.hbs",
+  },
+  
+  {
+    output: "./index-original.html",
+    content: {
+      title: "Our Goal",
+      description:
+        "This website is out to show you that our planet does not have a lot left if we dont change our ways of living.",
+      heading_icon: "pe-7s-car icon-gradient bg-mean-fruit",
+    },
     template: "./src/DemoPages/dashboards/analytics.hbs",
   },
+ 
   {
     output: "./analytics-variation.html",
     content: {
